8ac1098b4e80e33475792e4c13229ce45f81c35e
Thomas Malcher <malcher@student.tugraz.at>
2017-05-03 15:17:47 +0200

    add post for random sensors

--- framework/random_sensors/urls.py
+++ framework/random_sensors/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from .views import temperature, humidity
+from .views import temperature, humidity, outside_temperature
 
 
 urlpatterns = [
     url(r'humidity', humidity),
+    url(r'outside_temperature', outside_temperature),
     url(r'temperature', temperature),
+
 ]
--- framework/random_sensors/views.py
+++ framework/random_sensors/views.py
@@ -4,19 +4,49 @@ from datetime import datetime
 import random
 import math
 
-temperature_value = 20
+temperature_value = 25
+outside_temperature_value = 10
 humidity_value = 50
+
+import window.views as winview
 
 
 def temperature(request):
     global temperature_value
-    temperature_value = temperature_value + random.triangular(-1, 1)
+    global outside_temperature_value
+
+    if request.POST:
+        temperature_value = int(request.POST['value'])
+
+    if winview.window_open:
+        d = outside_temperature_value - temperature_value
+        temperature_value = temperature_value + d*random.random()*0.5
+    else:
+        temperature_value = temperature_value + random.triangular(-1, 1)
+
     temperature_value = min(max(temperature_value, 0), 35)
     return JsonResponse({'value': math.floor(temperature_value), 'time': datetime.now().isoformat()})
 
 
 def humidity(request):
     global humidity_value
-    humidity_value = humidity_value + random.triangular(-1, 1)
+    if request.POST:
+        humidity_value = int(request.POST['value'])
+
+    if winview.window_open:
+        humidity_value = humidity_value + random.triangular(-1, 0)
+    else:
+        humidity_value = humidity_value + random.triangular(-1, 1)
+
     humidity_value = min(max(humidity_value, 20), 90)
     return JsonResponse({'value': math.floor(humidity_value), 'time': datetime.now().isoformat()})
+
+
+def outside_temperature(request):
+    global outside_temperature_value
+    if request.POST:
+        outside_temperature_value = int(request.POST['value'])
+    outside_temperature_value = outside_temperature_value + random.triangular(-1, 1)
+    outside_temperature_value = min(max(outside_temperature_value, -10), 45)
+    return JsonResponse({'value': math.floor(outside_temperature_value), 'time': datetime.now().isoformat()})
+
